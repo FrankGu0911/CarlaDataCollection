a8b13f00af3ff406a9f74271bbcc22ee80547cee
FrankGu0911 <frank@frankgu.club>
2023-07-19 17:56:11 +0800

    Add data collection base code

---
+++ data_collection/bashs/weather-13/routes_town01_tiny.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+export DATA_ROOT=dataset
+export YAML_ROOT=data_collection/yamls
+export CARLA_ROOT=carla
+export CARLA_SERVER=${CARLA_ROOT}/CarlaUE4.sh
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
+export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg
+export PYTHONPATH=$PYTHONPATH:leaderboard
+export PYTHONPATH=$PYTHONPATH:leaderboard/team_code
+export PYTHONPATH=$PYTHONPATH:scenario_runner
+
+export LEADERBOARD_ROOT=leaderboard
+
+export CHECKPOINT_ENDPOINT=${DATA_ROOT}/weather-13/results/routes_town01_tiny.json
+export SAVE_PATH=${DATA_ROOT}/weather-13/data
+export TEAM_CONFIG=${YAML_ROOT}/weather-13.yaml
+export TRAFFIC_SEED=20026
+export CARLA_SEED=20026
+export SCENARIOS=${LEADERBOARD_ROOT}/data/scenarios/town01_all_scenarios.json
+export ROUTES=${LEADERBOARD_ROOT}/data/training_routes/routes_town01_tiny.xml
+export TM_PORT=20526
+export PORT=20026
+export HOST=localhost
+export CHALLENGE_TRACK_CODENAME=SENSORS
+export DEBUG_CHALLENGE=0
+export REPETITIONS=1 # multiple evaluation runs
+export TEAM_AGENT=${LEADERBOARD_ROOT}/team_code/auto_pilot.py # agent
+export RESUME=True
+
+python3 ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
+--scenarios=${SCENARIOS}  \
+--routes=${ROUTES} \
+--repetitions=${REPETITIONS} \
+--track=${CHALLENGE_TRACK_CODENAME} \
+--checkpoint=${CHECKPOINT_ENDPOINT} \
+--agent=${TEAM_AGENT} \
+--agent-config=${TEAM_CONFIG} \
+--debug=${DEBUG_CHALLENGE} \
+--record=${RECORD_PATH} \
+--resume=${RESUME} \
+--port=${PORT} \
+--host=${HOST} \
+--trafficManagerPort=${TM_PORT} \
+--carlaProviderSeed=${CARLA_SEED} \
+--trafficManagerSeed=${TRAFFIC_SEED}
---
+++ data_collection/batch_run/run_route_routes_town02_tiny.sh
@@ -0,0 +1,14 @@
+bash data_collection/bashs/weather-0/routes_town02_tiny.sh & 
+bash data_collection/bashs/weather-1/routes_town02_tiny.sh & 
+bash data_collection/bashs/weather-2/routes_town02_tiny.sh & 
+bash data_collection/bashs/weather-3/routes_town02_tiny.sh & 
+bash data_collection/bashs/weather-4/routes_town02_tiny.sh & 
+bash data_collection/bashs/weather-5/routes_town02_tiny.sh & 
+bash data_collection/bashs/weather-6/routes_town02_tiny.sh & 
+bash data_collection/bashs/weather-7/routes_town02_tiny.sh & 
+bash data_collection/bashs/weather-8/routes_town02_tiny.sh & 
+bash data_collection/bashs/weather-9/routes_town02_tiny.sh & 
+bash data_collection/bashs/weather-10/routes_town02_tiny.sh & 
+bash data_collection/bashs/weather-11/routes_town02_tiny.sh & 
+bash data_collection/bashs/weather-12/routes_town02_tiny.sh & 
+bash data_collection/bashs/weather-13/routes_town02_tiny.sh & 
