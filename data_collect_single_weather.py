04703acc71e4f0d37676d359e35c244d530445b8
FrankGu0911 <frank@frankgu.club>
2023-07-24 23:44:59 +0800

    Add logfile

--- data_collect_single_weather.py
+++ data_collect_single_weather.py
@@ -1,8 +1,9 @@
-import argparse,os,time,sys,logging
+import argparse,os,time,sys,logging,tqdm,datetime
 import subprocess
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 exception = ["routes_town01_long.sh"]
-
+if not os.path.exists('log'):
+    os.mkdir('log')
 def SetArgParser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--weather', type=int, default=0)
@@ -12,6 +13,9 @@ def SetArgParser():
     # parser.add_argument('--port', type=int, default=-1)
     args = parser.parse_args()
     return args
+
+def GetLocalTime():
+    return datetime.datetime.now().strftime('%m_%d_%H_%M_%S')
 
 class CarlaManager():
     def __init__(self,carla_root:str):
@@ -23,7 +27,8 @@ class CarlaManager():
     
     def RunCarla(self,port:int):
         carla_cmd = os.path.join(self.carla_root,'CarlaUE4/Binaries/Linux/CarlaUE4-Linux-Shipping CarlaUE4 -resx=800 -resy=600 -quality-level=Epic -fps=20 -world-port=%d' % port)
-        self.carla_process = subprocess.Popen(carla_cmd,shell=True)
+        logfile = open('log/carla_%s.log' % GetLocalTime(),'w')
+        self.carla_process = subprocess.Popen(carla_cmd,shell=True,stdout=logfile,preexec_fn=os.setsid)
         self.carla_pid = self.carla_process.pid
         self.carla_pgid = os.getpgid(self.carla_pid)
         self.carla_port = port
@@ -66,15 +71,17 @@ if __name__ == '__main__':
         bash_list.append(os.path.join(bash_base_path,bash))
     bash_list.sort()
     # print(bash_list)
-    for bash in bash_list:
+    for bash in tqdm.tqdm(bash_list):
         x_access = os.access(bash,os.X_OK)
         if not x_access:
             logging.info('%s is not executable' % bash)
             # +x
             logging.info('chmod +x %s' % bash)
             os.chmod(bash,os.stat(bash).st_mode | 0o111)
+        logfile_path = os.path.join("w%2d"%args.weather, os.path.basename(bash).split('.')[0]+GetLocalTime()+'.log')
+        logfile = open(logfile_path,'w')
         logging.info('Running bash %s' % bash)
-        subprocess.call(bash,shell=True)
+        subprocess.call(bash,shell=True,stdout=logfile)
         time.sleep(5)
     carla.StopCarla()
     
