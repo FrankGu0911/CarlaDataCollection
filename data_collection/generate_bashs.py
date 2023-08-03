f1b094aa9f70275a23af1961e2771396fb19978f
frank <frank@frankgu.club>
2023-08-03 16:10:58 +0800

    Format Generate Bashs

--- data_collection/generate_bashs.py
+++ data_collection/generate_bashs.py
@@ -2,54 +2,28 @@ import os
 import random
 
 routes = {}
-routes[
-    "training_routes/routes_town01_short.xml"
-] = "scenarios/town01_all_scenarios.json"
+routes["training_routes/routes_town01_short.xml"] = "scenarios/town01_all_scenarios.json"
 routes["training_routes/routes_town01_tiny.xml"] = "scenarios/town01_all_scenarios.json"
-routes[
-    "training_routes/routes_town02_short.xml"
-] = "scenarios/town02_all_scenarios.json"
+routes["training_routes/routes_town02_short.xml"] = "scenarios/town02_all_scenarios.json"
 routes["training_routes/routes_town02_tiny.xml"] = "scenarios/town02_all_scenarios.json"
-routes[
-    "training_routes/routes_town03_short.xml"
-] = "scenarios/town03_all_scenarios.json"
+routes["training_routes/routes_town03_short.xml"] = "scenarios/town03_all_scenarios.json"
 routes["training_routes/routes_town03_tiny.xml"] = "scenarios/town03_all_scenarios.json"
-routes[
-    "training_routes/routes_town04_short.xml"
-] = "scenarios/town04_all_scenarios.json"
+routes["training_routes/routes_town04_short.xml"] = "scenarios/town04_all_scenarios.json"
 routes["training_routes/routes_town04_tiny.xml"] = "scenarios/town04_all_scenarios.json"
-routes[
-    "training_routes/routes_town05_short.xml"
-] = "scenarios/town05_all_scenarios.json"
+routes["training_routes/routes_town05_short.xml"] = "scenarios/town05_all_scenarios.json"
 routes["training_routes/routes_town05_tiny.xml"] = "scenarios/town05_all_scenarios.json"
 routes["training_routes/routes_town05_long.xml"] = "scenarios/town05_all_scenarios.json"
-routes[
-    "training_routes/routes_town06_short.xml"
-] = "scenarios/town06_all_scenarios.json"
+routes["training_routes/routes_town06_short.xml"] = "scenarios/town06_all_scenarios.json"
 routes["training_routes/routes_town06_tiny.xml"] = "scenarios/town06_all_scenarios.json"
-routes[
-    "training_routes/routes_town07_short.xml"
-] = "scenarios/town07_all_scenarios.json"
+routes["training_routes/routes_town07_short.xml"] = "scenarios/town07_all_scenarios.json"
 routes["training_routes/routes_town07_tiny.xml"] = "scenarios/town07_all_scenarios.json"
-routes[
-    "training_routes/routes_town10_short.xml"
-] = "scenarios/town10_all_scenarios.json"
+routes["training_routes/routes_town10_short.xml"] = "scenarios/town10_all_scenarios.json"
 routes["training_routes/routes_town10_tiny.xml"] = "scenarios/town10_all_scenarios.json"
-routes[
-    "additional_routes/routes_town01_long.xml"
-] = "scenarios/town01_all_scenarios.json"
-routes[
-    "additional_routes/routes_town02_long.xml"
-] = "scenarios/town02_all_scenarios.json"
-routes[
-    "additional_routes/routes_town03_long.xml"
-] = "scenarios/town03_all_scenarios.json"
-routes[
-    "additional_routes/routes_town04_long.xml"
-] = "scenarios/town04_all_scenarios.json"
-routes[
-    "additional_routes/routes_town06_long.xml"
-] = "scenarios/town06_all_scenarios.json"
+routes["additional_routes/routes_town01_long.xml"] = "scenarios/town01_all_scenarios.json"
+routes["additional_routes/routes_town02_long.xml"] = "scenarios/town02_all_scenarios.json"
+routes["additional_routes/routes_town03_long.xml"] = "scenarios/town03_all_scenarios.json"
+routes["additional_routes/routes_town04_long.xml"] = "scenarios/town04_all_scenarios.json"
+routes["additional_routes/routes_town06_long.xml"] = "scenarios/town06_all_scenarios.json"
 
 ip_ports = []
 
@@ -77,7 +51,8 @@ def generate_script(
     lines.append("export CARLA_SEED=%d\n" % port)
     lines.append("export TRAFFIC_SEED=%d\n" % port)
     lines.append("export TEAM_CONFIG=${YAML_ROOT}/%s\n" % config_path)
-    lines.append("export SAVE_PATH=${DATA_ROOT}/%s/data\n" % config_path.split(".")[0])
+    lines.append(
+        "export SAVE_PATH=${DATA_ROOT}/%s/data\n" % config_path.split(".")[0])
     lines.append(
         "export CHECKPOINT_ENDPOINT=${DATA_ROOT}/%s/results/%s.json\n"
         % (config_path.split(".")[0], route.split("/")[1].split(".")[0])
@@ -108,7 +83,8 @@ for i in range(14):
             configs[i],
         )
         fw = open(
-            "bashs/weather-%d/%s.sh" % (i, route.split("/")[1].split(".")[0]), "w"
+            "bashs/weather-%d/%s.sh" % (i, route.split("/")
+                                        [1].split(".")[0]), "w"
         )
         for line in script:
             fw.write(line)
